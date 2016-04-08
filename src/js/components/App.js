b4a23d220e41d135714a0d651de745a7e1028b91
Andrew Fink <andrewfink8@gmail.com>
2016-04-08 16:32:59 -0700

    getting things up and running still

---
+++ src/js/components/App.js
@@ -0,0 +1,13 @@
+var React = reauire('react');
+var AppActions  = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+var App = React.createClass({
+    render: function(){
+      return(
+          <div>
+            the app
+          </div>
+      );
+    }
+});
